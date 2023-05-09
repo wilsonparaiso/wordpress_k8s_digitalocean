284ba5878982368f28255e95e452516092cd3cc5
wilson Paraiso <wilsonparaiso@gmail.com>
2023-05-08 21:35:08 -0300

    Add new resources

--- main.tf
+++ main.tf
@@ -22,9 +22,23 @@ resource "digitalocean_firewall" "wparaiso_firewall" {
     port_range       = "1-65535"
     source_addresses = ["192.168.0.0/16"]
   }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "1-65535"
+    source_addresses = [var.admin_ip_scope]
+  }
   tags = ["k8s"]
 }
 
+resource "digitalocean_database_firewall" "wparaiso_db_firewall" {
+  cluster_id = digitalocean_database_cluster.wparaiso_db.id
+
+  rule {
+    type  = "k8s"
+    value = digitalocean_kubernetes_cluster.wparaiso_cluster.id
+  }
+}
 
 resource "digitalocean_kubernetes_cluster" "wparaiso_cluster" {
   name     = var.digitalocean_kubernetes_cluster_name
@@ -45,16 +59,112 @@ resource "digitalocean_kubernetes_cluster" "wparaiso_cluster" {
 resource "local_file" "kubeconfig" {
   content  = digitalocean_kubernetes_cluster.wparaiso_cluster.kube_config[0].raw_config
   filename = "kubeconfig.yaml"
+  depends_on = [digitalocean_kubernetes_cluster.wparaiso_cluster]
+}
+
+resource "digitalocean_database_cluster" "wparaiso_db" {
+  name       = var.digitalocean_database_cluster_name
+  engine     = var.digitalocean_database_cluster_engine
+  version    = var.digitalocean_database_cluster_version
+  size       = var.digitalocean_database_cluster_size
+  region     = var.digitalocean_region
+  node_count = var.digitalocean_database_cluster_node_count
+  private_network_uuid = digitalocean_vpc.wparaiso_vpc.id
+}
+
+resource "digitalocean_database_user" "wordpress_user" {
+  cluster_id = digitalocean_database_cluster.wparaiso_db.id
+  name       = var.database_wordpress_user
+  depends_on = [digitalocean_database_cluster.wparaiso_db]
+}
+
+resource "digitalocean_database_db" "wordpress" {
+  cluster_id = digitalocean_database_cluster.wparaiso_db.id
+  name       = var.database_wordpress_name
+  depends_on = [digitalocean_database_cluster.wparaiso_db]
+}
+
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  depends_on = [digitalocean_kubernetes_cluster.wparaiso_cluster]
+}
+
+resource "digitalocean_domain" "wparaiso_domain" {
+  name       = "wparaiso.com"
+}
+
+resource "digitalocean_record" "wordpress_dns" {
+  domain = digitalocean_domain.wparaiso_domain.name
+  type   = "A"
+  name   = "www"
+  value  = digitalocean_kubernetes_cluster.wparaiso_cluster.endpoint
+  ttl    = 3600
+  depends_on = [helm_release.nginx_ingress]
+}
+
+resource "digitalocean_certificate" "wordpress_cert" {
+  name    = "le-wordpress-cert"
+  type    = "lets_encrypt"
+  domains = [digitalocean_domain.wparaiso_domain.name]
 }
 
 resource "helm_release" "wordpress" {
   name       = "wordpress"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "wordpress"
+  depends_on = [helm_release.nginx_ingress,digitalocean_database_user.wordpress_user,digitalocean_database_db.wordpress]
 
   set {
-    name  = "service.type"
-    value = "LoadBalancer"
+    name  = "mariadb.enabled"
+    value = "false"
   }
 
+  set {
+    name  = "externalDatabase.host"
+    value = digitalocean_database_cluster.wparaiso_db.host
+  }
+
+  set {
+    name  = "externalDatabase.user"
+    value = digitalocean_database_user.wordpress_user.name
+  }
+
+  set {
+    name  = "externalDatabase.password"
+    value = digitalocean_database_user.wordpress_user.password
+  }
+
+  set {
+    name  = "externalDatabase.database"
+    value = digitalocean_database_db.wordpress.name
+  }
+
+  set {
+    name  = "externalDatabase.port"
+    value = digitalocean_database_cluster.wparaiso_db.port
+  }
+
+  set { 
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+
+  set {
+    name  = "ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "ingress.annotations.kubernetes\\.io/ingress\\.class"
+    value = "nginx"
+  }
+
+  set {
+    name  = "ingress.hostname"
+    value = "wparaiso.com"
+  }
 }
+
--- variables.tf
+++ variables.tf
@@ -38,3 +38,49 @@ variable "digitalocean_kubernetes_cluster_node_pool_count" {
     default     = 1
 }
 
+variable "digitalocean_database_cluster_name" {
+    type        = string
+    description = "Database Cluster Name"
+    default     = "wparaiso-db"
+}
+
+variable "digitalocean_database_cluster_engine" {
+    type        = string
+    description = "Database Cluster Engine"
+    default     = "mysql"
+}
+
+variable "digitalocean_database_cluster_version" {
+    type        = string
+    description = "Database Cluster Version"
+    default     = "8"
+}
+
+variable "digitalocean_database_cluster_size" {
+    type        = string
+    description = "Database Cluster Size"
+    default     = "db-s-1vcpu-1gb"
+}
+
+variable "digitalocean_database_cluster_node_count" {
+    type        = number
+    description = "Database Cluster Node Count"
+    default     = 1
+}
+
+variable "admin_ip_scope" {
+    type        = string
+    description = "Administrative ip address scope"
+}
+
+variable "database_wordpress_name" {
+    type        = string
+    description = "Database Wordpress name"
+    default     = "wordpress"
+}
+
+variable "database_wordpress_user" {
+    type        = string
+    description = "Database wordpress user"
+    default     = "wordpress_user"
+}
